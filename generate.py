580ed79378c7c8e3ffa00400aeb35b0cb1a44c4b
Jorge <jorpramo@gmail.com>
2015-09-24 23:09:46 +0200

    a

--- generate.py
+++ generate.py
@@ -2,23 +2,14 @@ __author__ = 'jpradas'
 import random
 import pymongo
 import settings as set
-import numpy as np
+
 
 client = pymongo.MongoClient(set.MONGO_URI)
 db = client.rdp
 rest=db.restaurantes
-total=rest.find({}).count()
+num=random.randint(1, 1000)
 
-cursor=rest.find({"posicion": str(total)})
+cursor=rest.find({"posicion": str(num)})[0]
 
-for c in cursor:
-    print(c["nombre"])
-c=[]
-total=rest.find({}).sort("posicion",1)
-for t in total:
-    c.append(([t['posicion'].replace("\n", "").replace(".", ""), t["nombre"].replace("\n", "")]))
-
-c=sorted(c, key=lambda res: c[0] ,reverse=True)
-
-for lin in c:
-    print(lin[0] + ";" + lin[1])
+print(num)
+print(cursor['nombre'])
