850b23b0d9e2e3374c4f4f65c37b0b2254bf2b09
Jorge <jorpramo@gmail.com>
2015-10-02 00:08:32 +0200

    a

--- generate.py
+++ generate.py
@@ -7,9 +7,10 @@ import settings as set
 client = pymongo.MongoClient(set.MONGO_URI)
 db = client.rdp
 rest=db.restaurantes
-num=random.randint(1, 1000)
+num=random.randint(1, 2500)
 
 cursor=rest.find({"posicion": str(num)})[0]
-
+#cursor=rest.find({"posicion": '2008'})
+#print(cursor.count())
 print(num)
 print(cursor['nombre'])
