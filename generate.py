fba060cad4813cacb1b7a2d9c9d90982dcec707c
jorpramo@gmail.com <jorpramo@gmail.com>
2015-10-02 13:29:13 +0200

    Trabajo

--- generate.py
+++ generate.py
@@ -2,15 +2,40 @@ __author__ = 'jpradas'
 import random
 import pymongo
 import settings as set
+from flask import Flask, redirect, request, url_for
+from flask import render_template
+import logging
+import sys
 
 
-client = pymongo.MongoClient(set.MONGO_URI)
-db = client.rdp
-rest=db.restaurantes
-num=random.randint(1, 2500)
+app = Flask(__name__)
+# Create dummy secrey key so we can use sessions
+app.config['SECRET_KEY'] = '123456790'
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
-cursor=rest.find({"posicion": str(num)})[0]
-#cursor=rest.find({"posicion": '2008'})
-#print(cursor.count())
-print(num)
-print(cursor['nombre'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sorteo/')
+def sorteo():
+
+    client = pymongo.MongoClient(set.MONGO_URI)
+    db = client.rdp
+    rest=db.restaurantes
+
+    #cursor=rest.find({"posicion": num})[0]
+    conteo=0
+    while (conteo==0):
+        num=str(random.randint(1, 2500))
+        cursor=rest.find({"posicion": num})
+        conteo=cursor.count()
+    nombre=cursor[0]['nombre']
+    return render_template('sorteo.html', num=num, nombre=nombre)
+
+
+if __name__ == '__main__':
+    #admin = Admin(app, name='RandomDinnerProject: Listado', template_mode='bootstrap3')
+    app.run()
+
