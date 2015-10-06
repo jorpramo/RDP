a791d4fe17a186008102f64058b8fb065909ab1d
jorpramo@gmail.com <jorpramo@gmail.com>
2015-10-06 09:07:27 +0200

    Trabajo

--- admin.py
+++ admin.py
@@ -1,12 +1,12 @@
 __author__ = 'jpradas'
 
-
+import pymongo
 import scrapy
 from spiders.trip_Spider import tripSpider
 
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
-import settings
+import settings as set
 
 process = CrawlerProcess(get_project_settings())
 
